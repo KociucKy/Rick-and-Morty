f7f657e47cfe172649a6b5510237925bc0b11a51
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2023-07-25 17:30:17 +0200

    Add basic TabBar

--- RickAndMorty/RickAndMorty/AppEntry.swift
+++ RickAndMorty/RickAndMorty/AppEntry.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct RickAndMortyApp: App {
+struct AppEntry: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarView()
         }
     }
 }
---
+++ RickAndMorty/RickAndMorty/TabBarView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Kuba Kociucki on 25/07/2023.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @AppStorage("selectedTab") var selectedTab: Int = 0
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Characters")
+                .tag(0)
+                .tabItem { Label("Characters", systemImage: "person.3.fill") }
+            Text("Favorites")
+                .tag(1)
+                .tabItem { Label("Favorites", systemImage: "heart.fill") }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
